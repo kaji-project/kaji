722335af7837c2f6947a1373102980625f29eb45
Thibault Cohen <titilambert@gmail.com>
2014-10-25 01:33:39 -0400

    update influxdb commands

--- debian/influxdb.sh
+++ debian/influxdb.sh
@@ -9,9 +9,17 @@ curl -X POST 'http://localhost:8086/db?u=root&p=root' \
   -d '{"name": "grafanashinken"}'
 
 # add database user
+## kaji user
+curl -X POST 'http://localhost:8086/db/shinken/users?u=root&p=root' \
+  -d '{"name": "kaji", "password": "kaji"}'
+### Set kaji user as reader only
+curl -X POST 'http://localhost:8086/db/shinken/users/shinken?u=root&p=root' \
+  -d '{ "readFrom": ".*", "writeTo": "^$" }'
+## Shinken user
 curl -X POST 'http://localhost:8086/db/shinken/users?u=root&p=root' \
   -d '{"name": "shinken", "password": "shinken"}'
 
+## Set shinken user as writer only
 curl -X POST 'http://localhost:8086/db/shinken/users/shinken?u=root&p=root' \
   -d '{ "readFrom": "^$", "writeTo": ".*" }'
 
