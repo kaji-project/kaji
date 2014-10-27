ec01092014b7e4c324faee2431f2d7ef9324099c
Thibault Cohen <titilambert@gmail.com>
2014-10-26 23:21:11 -0400

    Change group and owner of shinken conf

--- scripts/kaji-shinken-init-config.sh
+++ scripts/kaji-shinken-init-config.sh
@@ -10,11 +10,8 @@ echo "Prepare Shinken/Adagiosconfig folder"
 # Git init /etc/shinken
 if [ ! -x /etc/shinken/.git ]
 then
-  chown -R shinken:shinken /etc/shinken/
   usermod -s /bin/bash shinken
-  usermod -G shinken www-data
-  chgrp -R www-data /etc/shinken
-  chmod -R g+w /etc/shinken
+  usermod -G www-data shinken
   cd /etc/shinken/
   #usermod -s /bin/bash www-data
   echo "[user]" > /var/www/.gitconfig
@@ -25,6 +22,9 @@ then
   su shinken -c 'git init'
   su shinken -c 'git add .'
   su shinken -c 'git commit -a -m "Initial Kaji installation commit"'
+  chown -R www-data:shinken /etc/shinken/
+  chmod -R g+w /etc/shinken
+  chgrp -R shinken /etc/shinken
 fi
 
 echo "Preparation DONE"
