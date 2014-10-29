882a88c58e92e1f6070c3bca3b8e7c38ff171a9b
Thibault Cohen <thibault.cohen@savoirfairelinux.com>
2014-10-29 13:49:56 -0400

    Delete useless commands for shinken init config

--- scripts/kaji-shinken-init-config.sh
+++ scripts/kaji-shinken-init-config.sh
@@ -11,21 +11,13 @@ echo "Prepare Shinken/Adagiosconfig folder"
 if [ ! -x /etc/shinken/.git ]
 then
   usermod -s /bin/bash shinken
-  usermod -G www-data shinken
   chown -R shinken:shinken /etc/shinken/
   cd /etc/shinken/
-  #usermod -s /bin/bash www-data
-  echo "[user]" > /var/www/.gitconfig
-  echo "        email = adagios@kaji-project.org" >> /var/www/.gitconfig
-  echo "        name = Adagios" >> /var/www/.gitconfig
   su shinken -c 'git config --global user.email "shinken@kaji-project.org"'
   su shinken -c 'git config --global user.name "shinken"'
   su shinken -c 'git init'
   su shinken -c 'git add .'
   su shinken -c 'git commit -a -m "Initial Kaji installation commit"'
-  chown -R www-data:shinken /etc/shinken/
-  chmod -R g+w /etc/shinken
-  chgrp -R shinken /etc/shinken
 fi
 
 echo "Preparation DONE"
